49b1309269adcd08c5aa38f981b39da93e915354
lin-o-a <102804581+lin-o-a@users.noreply.github.com>
2022-08-12 20:17:41 +0300

    Delete PropertyManagement directory

--- PropertyManagement/IssueManagement/Logic/IIssueController.cs
+++
@@ -1,7 +0,0 @@
-using REM.Logic.CoreManagement.SystemController;
-using System;
-
-public interface IIssueController {
-    void Change(Issue issue);
-    bool Create(Issue issue);
-}
--- PropertyManagement/IssueManagement/Logic/RelatedTaskManagement/ITaskController.cs
+++
@@ -1,7 +0,0 @@
-using REM.Logic.RealEstateManagement.PropertyManagement.IssueManagement.Models;
-using System;
-using System.Collections.Generic;
-
-public interface ITaskController {
-    Guid Create(Guid issueId, ICollection<Task> tasks);
-}
